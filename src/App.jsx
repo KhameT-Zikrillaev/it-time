fad54995cd67f89f5ed130293045071138e55136
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-12-07 21:05:12 +0500

    add 404 found

--- src/App.jsx
+++ src/App.jsx
@@ -2,60 +2,37 @@ import './index.css';
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import HeaderLayout from './layout/HeaderLayout';
-import MainLayout from './layout/MainLayout';
-import FooterLayout from './layout/FooterLayout';
 import Loading from './animations/loading';
 import CallButton from './components/CallButton';
 import ScrollToTop from './components/ScrollToTop';
-import { Routes, Route } from 'react-router-dom';
-import NotFound from './pages/NotFound';
+import RootRoute from './config/routes';
 
 function App() {
   const [loadingActive, setLoadingActive] = useState(true);
   const [layoutgActive, setLayoutActive] = useState(false);
-  //closed
 
-  // fist animation
+  // first animation
   setTimeout(() => {
     setLoadingActive(false)
   }, 2500)
-  //closed
 
   //second animation
   setTimeout(() => {
     setLayoutActive(true)
   }, 2500)
-  //closed
   
   // Инициализация AOS при монтировании компонента
   useEffect(() => {
     AOS.init({ duration: 1000, once: true }); // once: true - анимация срабатывает только один раз
   }, []);
 
-
-
   return (
-   <>
-      {loadingActive && <Loading />
-      }
-
-      {layoutgActive &&  
-      <Routes>
-        <Route path="/" element={
-          <div className="wrapper min-h-screen bg-gray-50">
-            <HeaderLayout/>
-            <MainLayout/>
-            <FooterLayout/>
-          </div>
-        } />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      }
-
+    <>
+      {loadingActive && <Loading />}
+      {layoutgActive && <RootRoute />}
       <CallButton />
       <ScrollToTop />
-   </>
+    </>
   );
 }
 
--- src/config/routes/index.jsx
+++ src/config/routes/index.jsx
@@ -7,6 +7,16 @@ import Courses from '../../pages/courses';
 import Mentors from '../../pages/mentors';
 import CourseDetail from '../../pages/courses/pages/CourseDetail';
 import NotFound from '../../pages/NotFound';
+import HeaderLayout from '../../layout/HeaderLayout';
+import FooterLayout from '../../layout/FooterLayout';
+
+const PageWrapper = ({ children }) => (
+  <div className="wrapper min-h-screen bg-gray-50">
+    <HeaderLayout />
+    {children}
+    <FooterLayout />
+  </div>
+);
 
 export const routes = [
   {
@@ -43,7 +53,11 @@ function RootRoute() {
   return (
     <Routes>
       {routes.map((route, index) => (
-        <Route key={index} path={route.path} element={route.element} />
+        <Route 
+          key={index} 
+          path={route.path} 
+          element={<PageWrapper>{route.element}</PageWrapper>} 
+        />
       ))}
     </Routes>
   );
