7317137c842599bf2e069908c576222b045ef6f5
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-12-09 17:05:44 +0500

    fixed

--- src/pages/home/pages/SecondAboutPage/modules/AboutCard.jsx
+++ src/pages/home/pages/SecondAboutPage/modules/AboutCard.jsx
@@ -48,7 +48,7 @@ const AboutCard = ({ title, description, icon: Icon }) => {
                 <p className="text-lg text-gray-800 leading-relaxed">
                   {description}
                   <span className="block text-sm text-gray-500 mt-2 text-right">
-                    {Icon.name.replace('Fa', '')}
+                    {Icon.name.replace('Fa', '').replace(/([A-Z])/g, ' $1').trim()}
                   </span>
                 </p>
                 <div className="absolute inset-0 bg-gradient-to-r from-transparent via-red-500/5 to-transparent highlight-scanner rounded-xl"></div>
